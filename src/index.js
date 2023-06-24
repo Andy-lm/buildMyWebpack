e33716ffbdb202f9bf62723badda84eab5550c04
Andy <njloveqd@foxmail.com>
2023-06-24 08:54:48 +0800

    feat: 初始化

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import add from "./add.js";
+import { minus } from "./minus.js";
+import add2 from './demo/x.js'
+
+const sum = add(1, 2);
+const sum2 = add2(3,4)
+const division = minus(2, 1);
+
+
+console.log(sum);
+console.log(sum2);
+console.log(division);
